922753146629ef3bc3c65deafefe1b1b75cb34f7
tuxmuzon <tuxmuzon@gmail.com>
2017-02-27 06:51:03 +0300

    main

--- main.cpp
+++ main.cpp
@@ -7,21 +7,14 @@
 #include <egais.h>
 
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
     QApplication a(argc, argv);
-
     good_input goods;
     goods.show();
     goods_card goodscard;
-
-
     QObject::connect(&goods,SIGNAL(double_click(QString)),&goodscard,SLOT(show()));
     QObject::connect(&goods,SIGNAL(double_click(QString)),&goodscard,SLOT(get_goods_card(QString)));
     QObject::connect(&goods,SIGNAL(new_good(QString)),&goodscard,SLOT(show()));
     QObject::connect(&goods,SIGNAL(new_good(QString)),&goodscard,SLOT(new_goods_card(QString)));
-
-
-
     return a.exec();
 }
